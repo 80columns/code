dc2a832970512040ec2445f82066219d963f0ef3
Dan Fort <dan@80columns.com>
2012-10-24 10:43:48 -0700

    Updating code

--- 2012.11.17/ProjectEulerP20.pl
+++ 2012.11.17/ProjectEulerP20.pl
@@ -1,5 +1,15 @@
 #!/usr/bin/perl
 
+# http://projecteuler.net/problem=20
+#
+# n! means n * (n − 1) * ... * 3 * 2 * 1
+#
+# For example, 10! = 10 * 9 * ... * 3 * 2 * 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 +
+# 0 + 0 = 27.
+#
+# Find the sum of the digits in the number 100!
+
 use Math::BigInt;
 
 # Calculate the factorial of a number iteratively
