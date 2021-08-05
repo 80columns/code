d776236b7d714b6cb89e83876e8ea74a8686695b
Patrick <pdf@80columns.com>
2021-08-04 23:51:07 -0400

    adding solution for product of numbers in array

---
+++ ProductOfNumbersInArray/main.py
@@ -0,0 +1,57 @@
+"""
+Given an array of integers, return a new array such that each element at
+index i of the new array is the product of all the numbers in the original
+array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be
+[120, 60, 40, 30, 24]. If our input was [3, 2, 1] the expected output would be
+[2, 3, 6].
+"""
+
+# Follow-up: what if you can't use division?
+def GenerateArrayProductWithoutDivision(arr):
+    product_stack = []
+    output_arr = [0 for x in range(len(arr))]
+
+    for x in range(0, len(arr) - 1):
+        for y in range(0, len(product_stack)):
+            product_stack[y] *= arr[x]
+
+        product_stack.append(arr[x])
+
+    # the last value in the output array is the product of indexes 0*1*...*n-1
+    output_arr[len(arr) - 1] = product_stack.pop(0)
+
+    for y in range(0, len(product_stack)):
+        product_stack[y] *= arr[len(arr) - 1]
+
+    product_stack.append(arr[len(arr) - 1])
+
+    for x in range(0, len(arr) - 1):
+        output_arr[x] = product_stack.pop(0)
+
+        for y in range(0, len(product_stack)):
+            product_stack[y] *= arr[x]
+
+    return output_arr
+
+def GenerateArrayProduct(arr):
+    product = 1
+
+    for x in arr:
+        product *= x
+
+    for i in range(0, len(arr)):
+        arr[i] = product // arr[i]
+
+    return arr
+
+def main():
+    print(GenerateArrayProductWithoutDivision([10, 2, 3, 4, 5]))
+    print(GenerateArrayProduct([10, 2, 3, 4, 5]))
+
+    print(GenerateArrayProductWithoutDivision([3, 2, 1]))
+    print(GenerateArrayProduct([3, 2, 1]))
+
+if __name__ == "__main__":
+    main()
